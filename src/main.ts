859c2cd60278c24f860477ad4d2baef71c7c1958
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-11-02 19:30:02 -0300

    Cambio el server default port y puesta en QA

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { AppModule } from './app.module';
 
+// Environment
+import { SERVER_PORT } from './environment/environment.settings';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
