497b18f383a10bc96a3d158d60ecd87a317e2f64
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2020-11-02 19:30:02 -0300

    Cambio el server default port y puesta en QA

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const environment_settings_1 = require("./environment/environment.settings");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(environment_settings_1.SERVER_PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
