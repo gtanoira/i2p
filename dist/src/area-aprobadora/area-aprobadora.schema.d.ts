477d59f61db525d77e421474cedc83a09da6bebb
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2021-02-11 17:39:40 -0300

    Github Actions first yml

--- dist/src/area-aprobadora/area-aprobadora.schema.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { Proveedor } from '../models/proveedor.model';
-export declare type AreaAprobadoraDocument = AreaAprobadora & Document;
-export declare class AreaAprobadora {
-    id: string;
-    name: string;
-    proveedores: Proveedor[];
-}
-export declare const AreaAprobadoraSchema: import("mongoose").Schema<any>;
