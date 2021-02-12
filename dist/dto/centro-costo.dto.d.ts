a8077f1cfe77d1a375cd751c80dfdd9b995dc886
Gonzalo Tanoira <gonzalo.mtanoira@gmail.com>
2021-02-11 23:44:57 -0300

    Delete dist directory

--- dist/dto/centro-costo.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateCentroCostoDto {
-    sapId: string;
-    name: string;
-}
--- dist/models/proveedor.model.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Proveedor {
-    sapId: string;
-    name: string;
-    sapCondPago?: string;
-}
